a849f453d0d960ade4b323f288e180eed69e9989
stellamax <kirtimukha@nate.com>
2023-07-27 23:34:26 +0900

    [u] img - intersectionobserver 기본

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 function Card(props) {
+	const imgRef = useRef()
+	const callback = () => {
+		console.log("callback")
+	}
+	const options= {}
+	const observer = new IntersectionObserver(callback, options)
+	observer.observe(imgRef.current)
 	return (
 		<div className="Card text-center">
-			<img src={props.image}/>
+			<img src={props.image} alt={`test`} ref={imgRef}/>
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
