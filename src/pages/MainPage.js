f769297eaaf0a60fc25e178137b9ae6d82cd05d8
stellamax <kirtimukha@nate.com>
2023-07-28 00:11:23 +0900

    [u] multiple img - at same intersecting

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 import BannerVideo from '../components/BannerVideo'
 import ThreeColumns from '../components/ThreeColumns'
 import TwoColumns from '../components/TwoColumns'
@@ -12,6 +12,28 @@ import main_parts from '../assets/main-parts.jpg'
 import main_styles from '../assets/main-styles.jpg'
 
 function MainPage(props) {
+
+	useEffect( () => {
+		const callback = ( entries, observer ) => {
+
+
+			entries.forEach ( entry => {
+				if ( entry.isIntersecting ) {
+					console.log ( "here" )
+					entry.target.src = entry.target.dataset.src;
+					observer.unobserve ( entry.target );
+				}
+			})
+		}
+		const options = {}
+		const observer = new IntersectionObserver ( callback, options );
+		let target = '.observeImg';
+		document.querySelectorAll(target).forEach( (i) => {
+			if(i){
+				observer.observe(i);
+			}
+		})
+	}, [] );
 	return (
 		<div className="MainPage -mt-16">
 			<BannerVideo/>
@@ -26,7 +48,7 @@ function MainPage(props) {
 				<TwoColumns
 					bgColor={'#f4f4f4'}
 					columns={[
-						<img src={main_items} />,
+						<img data-src={main_items} alt={`Items`} className={`observeImg`}/>,
 						<Meta
 							title={'Items'}
 							content={'롱보드는 기본적으로 데크가 크기 때문에 입맛에 따라 정말 여러가지로 변형된 형태가 나올수 있습니다. 실제로 데크마다 가지는 모양, 재질, 무게는 천차만별인데, 본인의 라이딩 스타일에 맞춰 롱보드를 구매하시는게 좋습니다.'}
@@ -42,14 +64,14 @@ function MainPage(props) {
 							content={'롱보드는 데크, 트럭, 휠, 킹핀, 베어링 등 여러 부품들로 구성됩니다. 롱보드를 타다보면 조금씩 고장나는 부품이 있기 마련인데, 이럴때를 위해 롱보들의 부품들에 대해서 알고 있으면 큰 도움이 됩니다.'}
 							btnLink={'/part'}
 						/>,
-						<img src={main_parts} />
+						<img data-src={main_parts} alt='Parts of Longboard' className={`observeImg`} />,
 					]}
 					mobileReverse={true}
 				/>
 				<TwoColumns
 					bgColor={'#f4f4f4'}
 					columns={[
-						<img src={main_styles} />,
+						<img data-src={main_styles}  alt='Riding Styles' className={`observeImg`} />,
 						<Meta
 							title={'Riding Styles'}
 							content={'롱보드 라이딩 스타일에는 크게 프리스타일, 다운힐, 프리라이딩, 댄싱이 있습니다. 보통 롱보드는 라이딩 스타일에 따라 데크의 모양이 조금씩 달라집니다. 많은 롱보드 매니아들이 각 쓰임새에 맞는 보드들을 소유하고 있습니다.'}
